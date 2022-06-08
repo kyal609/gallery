3241e9255e0a00d74c2923a27767613e7a61aff8
Zayyar kyaw <106764897+kyal609@users.noreply.github.com>
2022-06-08 21:22:54 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var stt =0;
+let body =document.getElementById("body");
+let main =document.getElementById("main");
+let gallery=document.getElementsByClassName("gallery");
+function wallAnit() {
+    stt +=-379;
+    document.getElementById("wall").style.left=stt;
+    if(stt==-3032){
+        stt=379;
+    }
+}
+  body.onscroll =function(){
+    if(body.scrollTop>270){
+        main.style.overflow="scroll";
+    }
+    else{
+        main.style.overflow="hidden";
+    }
+}
+gallery[0].onclick=function(){
+      location.replace("converter.html");
+     }
+gallery[1].onclick=function(){
+    document.getElementById("load").style.display="block";
+}
